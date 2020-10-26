a39ecce50257cde0aa872fcea3d28cbb5e37f9b6
Alex Robinson <alexandra.scott335@gmail.com>
2020-10-26 13:15:13 -0600

    add base code

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to '/'
+    else
+      flash.now[:error] = "Sorry, your credentials are bad."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash.now[:success] = 'You have successfully logged out!'
+    redirect_to '/'
+  end
+end
